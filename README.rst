b5580f0b8370687c3b6ecf84969288b0617c5f6b
mike spindel <deactivated@gmail.com>
2010-12-14 11:28:39 -0800

    Initial commit

---
+++ README.rst
@@ -0,0 +1,35 @@
+==========================================================
+python-iso3166 - Standalone ISO 3166-1 country definitions
+==========================================================
+
+:Authors:
+        Mike Spindel
+:Version: 0.1
+
+
+ISO 3166-1 defines two-letter, three-letter, and three-digit country
+codes.  `python-iso3166` is a self-contained module that converts
+between these codes and the corresponding country name.
+
+
+Installation
+============
+
+::
+
+  $ pip install iso3166
+
+
+Usage
+=====
+
+::
+
+  >>> from iso3166 import countries
+  >>>
+  >>> countries.get('us')
+  Country(name=u'United States', alpha2='US', alpha3='USA', numeric='840')
+  >>> countries.get('ala')
+  Country(name=u'\xc5land Islands', alpha2='AX', alpha3='ALA', numeric='248')
+  >>> countries.get(8)
+  Country(name=u'Albania', alpha2='AL', alpha3='ALB', numeric='008')
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os.path
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+    
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='iso3166',
+      version="0.1",
+      author="Mike Spindel",
+      author_email="deactivated@gmail.com",
+      license="MIT",
+      keywords="iso 3166-1 country codes",
+      url="http://github.com/deactivated/python-iso3166",
+      description='Self-contained ISO 3166-1 country definitions.',
+      packages=find_packages(exclude=['ez_setup']),
+      long_description=read('README.rst'),
+      zip_safe=False,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers",
+          "Natural Language :: English",
+          "Programming Language :: Python"])
