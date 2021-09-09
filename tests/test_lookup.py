850bc5a9d94ffde671eff8cae8525146f16f8b24
Rick Voormolen <rick@scriptix.io>
2021-09-09 15:06:21 +0200

    Add typing to tests, improve tests

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from typing import List, Union
 import pytest
 import iso3166
 from iso3166 import countries
 
 
-def check_lookup(alpha2, matching_keys, missing_keys):
+def check_lookup(
+    alpha2: str,
+    matching_keys: List[Union[int, str]],
+    missing_keys: List[Union[int, str]],
+) -> None:
     for k in matching_keys:
         assert countries[k].alpha2 == alpha2
         assert countries.get(k).alpha2 == alpha2
@@ -21,45 +26,45 @@ def check_lookup(alpha2, matching_keys, missing_keys):
         assert countries.get(k, None) is None
 
 
-def test_length():
+def test_length() -> None:
     assert len(countries) == len(iso3166._records)
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     check_lookup("US", ["us", "US"], [""])
 
 
-def test_none():
-    check_lookup("US", ["us", "US"], [None])
-
-
-def test_alpha2():
+def test_alpha2() -> None:
     check_lookup("US", ["us", "US"], ["zz"])
 
 
-def test_alpha3():
+def test_alpha3() -> None:
     check_lookup("US", ["usa", "USA"], ["zzz"])
 
 
-def test_name():
-    check_lookup("US",
-                 ["united states of america", "United STates of America"],
-                 ["zzzzz"])
+def test_name() -> None:
+    check_lookup(
+        "US", ["united states of america", "United STates of America"], ["zzzzz"]
+    )
 
 
-def test_numeric():
+def test_numeric() -> None:
     check_lookup("US", [840, "840"], [111, "111"])
 
     with pytest.raises(KeyError):
         countries.get("000")
 
 
-def test_alt_name():
+def test_alt_name() -> None:
     check_lookup("TW", ["taiwan", "Taiwan, province of china"], ["zzzzz"])
     check_lookup("PS", ["palestine", "palestine, state of"], ["zzzz"])
 
 
-def test_data():
+def test_none_default() -> None:
+    assert countries.get("NOTUS", None) is None
+
+
+def test_data() -> None:
     assert len(list(countries)) > 0
 
     for country in countries:
